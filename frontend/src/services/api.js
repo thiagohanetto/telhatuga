8c4acc9151710c50b9b52c92695debc3a2e13391
thiagohanetto <netto_thiago@hotmail.com>
2026-05-29 05:14:34 +0100

    Configura front end para producao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+baseURL: "https://telhatuga-backend.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
